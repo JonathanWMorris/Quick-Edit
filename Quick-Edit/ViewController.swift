f8b4da1e204f67bb40d2b1616ba45a36c71fa6fd
Jonathan Morris <jonathan_wellington@icloud.com>
2021-05-13 18:58:22 -0700

    Initial Commit

---
+++ Quick-Edit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Quick-Edit
+//
+//  Created by Jonathan Morris on 5/13/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
